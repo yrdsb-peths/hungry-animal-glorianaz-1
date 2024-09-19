b6c7f5e70317f1b4993ce1d1aa1310ab5fe6445e
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-09-19 12:49:43 -0400

    Restore Snake

--- Snake.java
+++ Snake.java
@@ -12,7 +12,7 @@ public class Snake extends Actor
      * Act - do whatever the Snake wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
+    public void act() 
     {
         // Add your action code here.
         
