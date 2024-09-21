63c00d88cbe68e782b45827c4b681d8f5f65394b
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-09-21 01:43:55 -0400

    Increasing difficulty with speed of Cupcake

--- Cupcake.java
+++ Cupcake.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Cupcake extends Actor
 {
+    int speed = 1;
     /**
      * Act - do whatever the Cupcake wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -16,7 +17,7 @@ public class Cupcake extends Actor
     {
         // Cupcake falls downwards
         int x = getX();
-        int y = getY() + 2;
+        int y = getY() + speed;
         setLocation(x, y);
         
         MyWorld world = (MyWorld) getWorld();
@@ -25,5 +26,10 @@ public class Cupcake extends Actor
             world.gameOver();
             world.removeObject(this);
         }
+    }
+    
+    public void setSpeed(int spd)
+    {
+        speed = spd;
     }
 }
